3b662c1985b7e90a531e0a9226c3eafd6ea0b4d4
rizasif <rizasif@gmail.com>
2018-12-30 01:43:03 +0100

    fipa

--- fipa.py
+++ fipa.py
@@ -3,6 +3,9 @@ from pade.core.agent import Agent
 from pade.acl.aid import AID
 from pade.acl.messages import ACLMessage
 
+agent_r_name = 'agente_r_{}@localhost:{}'.format(9001, 9001)
+agent_s_name = 'agente_r_{}@localhost:{}'.format(9002, 9002)
+
 class Sender(Agent):
     def __init__(self,aid):
         super(Sender, self).__init__(aid=aid)
@@ -11,7 +14,7 @@ class Sender(Agent):
     def on_start(self):
         display_message(self.aid.localname, 'Sending Message to receiver')
         message = ACLMessage(ACLMessage.INFORM)
-        message.add_receiver(AID('receiver'))
+        message.add_receiver(AID(agent_r_name))
         message.set_content('Hi')
         self.send(message)
 
@@ -27,8 +30,8 @@ class Receiver(Agent):
         display_message(self.aid.localname, 'Received Message: ' + message)
 
 if __name__ == '__main__':
-    agent_r = Receiver(AID(name="receiver"))
-    agent_s = Sender(AID(name="sender"))
+    agent_r = Receiver(AID(name=agent_r_name))
+    agent_s = Sender(AID(name=agent_s_name))
 
     start_loop([agent_r, agent_s])
 
